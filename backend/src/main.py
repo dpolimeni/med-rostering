0de9bb12fa3789edee9fadfd247e464f579ebc7f
dpolimeni <diego.polimeni99@gmail.com>
2025-02-18 22:58:25 +0100

    allow cors

--- backend/src/main.py
+++ backend/src/main.py
@@ -68,7 +68,7 @@ app.include_router(departments_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Your frontend URL
+    allow_origins=["*"],  # Your frontend URL
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
